fe37e8b1a188f450cf72148f4ef69ab0609a442d
Sebastian Edwards <me@sebastianedwards.co.nz>
2021-05-03 17:32:30 +0800

    Update snippets.

--- sc-payment-account-rs/interaction/devnet.snippets.sh
+++ sc-payment-account-rs/interaction/devnet.snippets.sh
@@ -5,21 +5,26 @@ DEPLOY_ARGUMENTS="0x0139472eff6886771a982f3083da5d421f24c29181e63888228dc81ca60d
 DEPLOY_GAS="80000000"
 
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} \
-          --gas-limit=${DEPLOY_GAS} --arguments ${DEPLOY_ARGUMENTS} \
-          --outfile="deploy-devnet.interaction.json" --send || return
+  erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} \
+        --gas-limit=${DEPLOY_GAS} --arguments ${DEPLOY_ARGUMENTS} \
+        --outfile="deploy-devnet.interaction.json" --send || return
 
-    TRANSACTION=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['hash']")
-    ADDRESS=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['address']")
+  TRANSACTION=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['hash']")
+  ADDRESS=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['address']")
 
-    erdpy data store --key=address-devnet --value=${ADDRESS}
-    erdpy data store --key=deployTransaction-devnet --value=${TRANSACTION}
+  erdpy data store --key=address-devnet --value=${ADDRESS}
+  erdpy data store --key=deployTransaction-devnet --value=${TRANSACTION}
 
-    echo ""
-    echo "Smart contract address: ${ADDRESS}"
+  echo ""
+  echo "Smart contract address: ${ADDRESS}"
 }
 
 checkDeployment() {
-    erdpy tx get --hash=$DEPLOY_TRANSACTION --omit-fields="['data', 'signature']"
-    erdpy account get --address=$ADDRESS --omit-fields="['code']"
+  erdpy tx get --hash=$DEPLOY_TRANSACTION --omit-fields="['data', 'signature']"
+  erdpy account get --address=$ADDRESS --omit-fields="['code']"
+}
+
+deposit() {
+  erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+        --gas-limit=${DEPLOY_GAS} --value=5000 --function=deposit --send
 }

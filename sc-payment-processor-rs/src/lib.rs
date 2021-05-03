baec7fbb6caaf4f592d9bcdb815a8c52873f351f
Sebastian Edwards <me@sebastianedwards.co.nz>
2021-05-03 20:07:45 +0800

    Store payment for future payout after settlement.

--- sc-payment-processor-rs/src/lib.rs
+++ sc-payment-processor-rs/src/lib.rs
@@ -6,13 +6,22 @@ elrond_wasm::derive_imports!();
 
 #[derive(TopEncode, TopDecode, PartialEq, TypeAbi)]
 pub struct WithdrawalLockKey {
-	payment_account_address: Address,
 	authorization_id: BoxedBytes,
+	payment_account_address: Address,
 }
+
+#[derive(TopEncode, TopDecode, PartialEq, TypeAbi)]
+pub struct Payment<BigUint: BigUintApi> {
+	amount: BigUint,
+	held_until: u64,
+	token: TokenIdentifier,
+}
+
+const PAYMENT_HOLD_PERIOD: u64 = 10 * 60 * 24;
 
 #[elrond_wasm_derive::callable(PaymentAccountProxy)]
 pub trait PaymentAccount {
-	fn requestPayment(&self, authorization_id: BoxedBytes, amount: BigUint, payment_id: BoxedBytes) -> ContractCall<BigUint, ()>;
+	fn requestPayment(&self, authorization_id: BoxedBytes, amount: BigUint) -> ContractCall<BigUint, ()>;
 }
 
 #[elrond_wasm_derive::contract(PaymentProcessorImpl)]
@@ -39,10 +48,18 @@ pub trait PaymentProcessor {
 		let payment_account = contract_call!(self, payment_account_address, PaymentAccountProxy);
 
 		payment_account
-			.requestPayment(authorization_id, amount, payment_id)
+			.requestPayment(authorization_id, amount.clone())
 			.execute_on_dest_context(self.blockchain().get_gas_left(), self.send());
 
 		// TODO: Remove withdrawal lock after callback success
+
+		let payment = Payment::<BigUint> {
+			amount: amount.clone(),
+			held_until: self.blockchain().get_block_nonce() + PAYMENT_HOLD_PERIOD,
+			token: TokenIdentifier::egld(), // TODO: Placeholder... get this from payment account authorization or pass in and cross-check in remote call?
+		};
+
+		self.payments().insert(payment_id, payment);
 
 		Ok(())
 	}
@@ -52,6 +69,9 @@ pub trait PaymentProcessor {
 	#[view(getOwner)]
 	#[storage_mapper("owner")]
 	fn owner(&self) -> SingleValueMapper<Self::Storage, Address>;
+
+	#[storage_mapper("payments")]
+	fn payments(&self) -> MapMapper<Self::Storage, BoxedBytes, Payment<BigUint>>;
 
 	#[storage_mapper("withdrawal_locks")]
 	fn withdrawal_locks(&self) -> MapMapper<Self::Storage, WithdrawalLockKey, BigUint>;

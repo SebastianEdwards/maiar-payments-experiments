d001691e4f7351ced06e0025d7f29fb2aecdaefd
Sebastian Edwards <me@sebastianedwards.co.nz>
2021-05-09 13:37:57 +0800

    Implement basic staking and slashing for processors.

--- sc-payment-processor-rs/src/lib.rs
+++ sc-payment-processor-rs/src/lib.rs
@@ -17,7 +17,9 @@ pub struct Payment<BigUint: BigUintApi> {
 	token: TokenIdentifier,
 }
 
-const PAYMENT_HOLD_PERIOD: u64 = 10 * 60 * 24;
+const MINIMUM_STAKE:        u64 = 5_000_000_000_000_000_000; // 5 EGLD
+const PAYMENT_HOLD_PERIOD:  u64 = 10 * 60 * 24; // ~24 hours
+const EARLY_PAYOUT_PENALTY: u64 = 500_000_000_000_000_000; // 0.5 EGLD
 
 #[elrond_wasm_derive::callable(PaymentAccountProxy)]
 pub trait PaymentAccount {
@@ -38,20 +40,11 @@ pub trait PaymentProcessor {
 
 		require!(amount > BigUint::zero(), "Payout amount must be greater than zero");
 
+		require!(self.is_staked(), "Contract not sufficiently staked");
+
 		let mut total_payable = self.payable_amount(&token).get();
 
-		if total_payable >= amount {
-			self.payable_amount(&token).set(&(&total_payable - &amount));
-
-			let _ = self.send().direct_esdt_via_transf_exec(
-				&payout_address,
-				token.as_esdt_identifier(),
-				&amount,
-				&[],
-			);
-
-			Ok(())
-		} else {
+		if total_payable < amount {
 			let mut held_amounts = self.held_amounts(&token);
 
 			require!(held_amounts.len() > 0, "No payments awaiting payout for token");
@@ -61,23 +54,28 @@ pub trait PaymentProcessor {
 			while match held_amounts.front() { Some((held_until, _)) => held_until >= current_epoch, None => false } {
 				total_payable += held_amounts.pop_front().unwrap().1;
 			}
+		}
 
+		if total_payable >= amount {
 			self.payable_amount(&token).set(&(&total_payable - &amount));
 
-			let _ = self.send().direct_esdt_via_transf_exec(
-				&payout_address,
-				token.as_esdt_identifier(),
-				&amount,
-				&[],
-			);
+			self.send_tokens(&token, &amount, &payout_address);
+		} else {
+			self.payable_amount(&token).set(&total_payable);
 
-			Ok(())
+			self.staked_amount().set(&(self.staked_amount().get() - BigUint::from(EARLY_PAYOUT_PENALTY)));
+
+			// TODO: Actually send slashed amount somewhere useful like community fund
 		}
+
+		Ok(())
 	}
 
 	#[endpoint(requestPayment)]
 	fn request_payment(&self, payment_account_address: Address, authorization_id: BoxedBytes, amount: BigUint) -> SCResult<AsyncCall<BigUint>> {
 		only_owner!(self, "Only owner may request payment");
+
+		require!(self.is_staked(), "Contract not sufficiently staked");
 
 		require!(amount > BigUint::zero(), "Requested amount must be greater than zero");
 
@@ -105,6 +103,35 @@ pub trait PaymentProcessor {
 		}
 	}
 
+	#[payable("EGLD")]
+	#[endpoint]
+	fn stake(&self, #[payment] amount: BigUint) -> SCResult<()> {
+		only_owner!(self, "Only owner may stake");
+
+		require!(amount > BigUint::zero(), "Stake amount must be greater than zero");
+
+		self.staked_amount().set(&(self.staked_amount().get() + amount));
+
+		Ok(())
+	}
+
+	#[view(isStaked)]
+	fn is_staked(&self) -> bool {
+		self.staked_amount().get() >= BigUint::from(MINIMUM_STAKE)
+	}
+
+	#[inline]
+	fn send_tokens(&self, token: &TokenIdentifier, amount: &BigUint, destination: &Address) {
+		if amount > &0 {
+			let _ = self.send().direct(
+				destination,
+				token,
+				amount,
+				&[],
+			);
+		}
+	}
+
 	// storage
 
 	#[view(getOwner)]
@@ -116,6 +143,10 @@ pub trait PaymentProcessor {
 
 	#[storage_mapper("payable_amount")]
 	fn payable_amount(&self, token: &TokenIdentifier) -> SingleValueMapper<Self::Storage, BigUint>;
+
+	#[view(getStakedAmount)]
+	#[storage_mapper("staked_amount")]
+	fn staked_amount(&self) -> SingleValueMapper<Self::Storage, BigUint>;
 
 	#[view(getUnsettledAmount)]
 	#[storage_mapper("unsettled_amount")]
